547bf8bce45a66a8c0f591b135eb2a4bd14d9584
Cristián Rojas <injcristianrojas@gmail.com>
2014-03-20 19:28:06 -0300

    Added GET parameter parsing functionality

--- server.py
+++ server.py
@@ -1,21 +1,27 @@
 import SimpleHTTPServer
 import SocketServer
+import urlparse
 from BaseHTTPServer import BaseHTTPRequestHandler
+from pprint import pprint
 
 class GetHandler(BaseHTTPRequestHandler):
-	
-	def do_GET(self):
-		self.send_response(200)
-		self.end_headers()
-		user_agent = str(self.headers['user-agent'])
-		print(user_agent)
-		self.wfile.write("User-Agent: %s" % user_agent)
-		return
+  
+  def do_GET(self):
+    self.send_response(200)
+    self.end_headers()
+    param_dict = {}
+    path = self.path
+    if '?' in path:
+      path, tmp = path.split('?', 1)
+      param_dict = urlparse.parse_qs(tmp)
+    pprint(param_dict)
+    self.wfile.write('Data received')
+    return
 
 if __name__ == '__main__':
-	from BaseHTTPServer import HTTPServer
-	port = 8000
-	server = HTTPServer(('', port), GetHandler)
-	address_tuple = server.server_address
-	print 'Starting server at http://%s:%s, use <Ctrl-C> to stop' % address_tuple
-	server.serve_forever()
+  from BaseHTTPServer import HTTPServer
+  port = 8000
+  server = HTTPServer(('', port), GetHandler)
+  address_tuple = server.server_address
+  print 'Starting server at http://%s:%s, use <Ctrl-C> to stop' % address_tuple
+  server.serve_forever()
